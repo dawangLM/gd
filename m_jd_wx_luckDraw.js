5403a1535dc2f22dd2900e1cf0600fa7ee8b46f2
knowledgeablelmh <83534987+knowledgeablelmh@users.noreply.github.com>
2022-06-04 02:14:32 +0800

    Update m_jd_wx_luckDraw.js

--- m_jd_wx_luckDraw.js
+++ m_jd_wx_luckDraw.js
@@ -225,5 +225,5 @@ $.after = async function () {
     $.msg.push(message)
     $.msg.push($.activityUrl);
 }
-$.run({whitelist: ['1-5'], wait: [3000, 5000]}).catch(
+$.run({whitelist: ['1-99'], wait: [3000, 5000]}).catch(
     reason => $.log(reason));
